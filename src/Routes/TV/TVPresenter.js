f4e2fd35189665186d2fe3ed7d8ac6d76aeb6cd4
lattepapa <whitelatte46@gmail.com>
2021-08-09 12:58:06 +0900

    modify TVPresenter to setup Proptypes of props

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,50 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "Components/Section";
+import Loader from "Components/Loader";
+
+const Container = styled.div`
+    padding: 0 10px;
+`;
+
+const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
+    loading ? (<Loader />) : (
+        <Container>
+            {/* Top Rated TV Shows Section */}
+            {topRated && topRated.length > 0 &&
+                <Section title="Top Rated TV Shows">
+                    {topRated.map(show =>
+                        <span key={show.id}>
+                            {show.name}
+                        </span>)}
+                </Section>}
+
+            {/* Popular TV Shows Section */}
+            {popular && popular.length > 0 &&
+                <Section title="Popular TV Shows">
+                    {popular.map(show =>
+                        <span key={show.id}>
+                            {show.name}
+                        </span>)}
+                </Section>}
+
+            {/* Airing Today Section */}
+            {airingToday && airingToday.length > 0 &&
+                <Section title="Airing Today">
+                    {airingToday.map(show =>
+                        <span key={show.id}>
+                            {show.name}
+                        </span>)}
+                </Section>}
+        </Container>);
+
+TVPresenter.propTypes = {
+    topRated: PropTypes.array,
+    popular: PropTypes.array,
+    airingToday: PropTypes.array,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string
+}
+
+export default TVPresenter;
