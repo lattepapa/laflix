546d7dd5619805bbd8cf639f0ded3f0cc666b5c2
lattepapa <whitelatte46@gmail.com>
2021-08-11 19:28:26 +0900

    add detail posters and styles

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,69 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
 
-const DetailPresenter = ({ result, loading, error }) => null;
+const Container = styled.div`
+    height: calc(100vh - 50px);
+    width: 100%;
+    position: relative;
+    padding: 50px;
+`;
+
+const Backdrop = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${props => props.bgImage});
+    background-position: center center;
+    background-size: cover;
+    filter: blur(3px);
+    opacity: 0.7;
+    z-index: 0;
+`;
+
+const Content = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    z-index: 1;
+`;
+
+const Cover = styled.div`
+    width: 30%;
+    height: 100%;
+    background-image: url(${props => props.bgImage});
+    background-position: center center;
+    background-size: cover;
+    border-radius: 5px;
+`;
+
+const Data = styled.div`
+    width: 70%;
+    margin-left: 10px;
+`;
+
+const Title = styled.span``;
+
+const DetailPresenter = ({ result, loading, error }) => (
+    loading ?
+        <Loader /> :
+        <Container>
+            <Backdrop
+                bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+            />
+            <Content>
+                <Cover
+                    bgImage={result.poster_path ?
+                        `https://image.tmdb.org/t/p/original${result.backdrop_path}` :
+                        require("../../Assets/noposter.png")}
+                />
+            </Content>
+        </Container>
+);
 
 DetailPresenter.propTypes = {
     result: PropTypes.object,
