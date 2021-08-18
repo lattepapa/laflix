71869e7097f1c859cc076f3b6da4813040a56bc6
lattepapa <whitelatte46@gmail.com>
2021-08-18 14:03:47 +0900

    add detail info of the content

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -46,23 +46,94 @@ const Data = styled.div`
     margin-left: 10px;
 `;
 
-const Title = styled.span``;
+// span 태그는 margin을 가지지 않으므로 h3 태그로 변경
+const Title = styled.h3`
+    font-size: 32px;
+    margin-bottom: 20px;
+`;
+
+const ItemContainer = styled.div`
+    margin: 20px 0;
+`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`
+    margin: 0 10px;
+`;
+
+const Overview = styled.p`
+    font-size: 12px;
+    opacity: 0.7;
+    line-height: 1.5;
+    width: 50%;
+`;
 
 const DetailPresenter = ({ result, loading, error }) => (
-    loading ?
-        <Loader /> :
-        <Container>
-            <Backdrop
-                bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+    loading
+    ? <Loader />
+    : <Container>
+        {/* 컨텐츠 백그라운드 이미지 */}
+        <Backdrop
+            bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+        />
+
+        {/* 컨텐츠 정보 */}
+        <Content>
+            {/* 영화 포스터 */}
+            <Cover
+                bgImage={
+                    result.poster_path
+                    ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+                    : require("../../Assets/noposter.png")
+                }
             />
-            <Content>
-                <Cover
-                    bgImage={result.poster_path ?
-                        `https://image.tmdb.org/t/p/original${result.backdrop_path}` :
-                        require("../../Assets/noposter.png")}
-                />
-            </Content>
-        </Container>
+
+            {/* 영화 정보 */}
+            <Data>
+                <Title>
+                    {
+                        result.original_title
+                        ? result.original_title
+                        : result.original_name
+                    }
+                </Title>
+                <ItemContainer>
+                    <Item>
+                        {
+                            result.release_date
+                            ? result.release_date.substring(0, 4)
+                            : result.first_air_date.substring(0, 4)
+                        }
+                    </Item>
+                    
+                    <Divider>·</Divider>
+                    
+                    <Item>
+                        {
+                            result.runtime
+                            ? result.runtime
+                            : result.episode_run_time[0]
+                        } min
+                    </Item>
+                    
+                    <Divider>·</Divider>
+
+                    <Item>
+                        {
+                            result.genres
+                            && result.genres.map((genre, index) =>
+                                index === result.genres.length - 1
+                                ? genre.name
+                                : `${genre.name} | `)
+                        }
+                    </Item>
+                </ItemContainer>
+
+                <Overview>{result.overview}</Overview>
+            </Data>
+        </Content>
+    </Container>
 );
 
 DetailPresenter.propTypes = {
