d7747ce7e77afa78e1e247f278e1e12acf64d707
lattepapa <whitelatte46@gmail.com>
2021-09-07 17:54:31 +0900

    modify scripting import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'Components/App';
+import { WDOTInitialization } from "dop-website-sdk";
+WDOTInitialization.setConfig({
+    serviceNumber:20045,
+    dotAccessToken:"AgaCWt+E9pKnx5vijC6ZBQ1Eu7eyI7SKp7KIHoxbthlhH08GoUum0K2kmJg7/HuRARcv8D89eyGNzszJgiIzdg==",
+    combackUserLimitDays:14,
+    dotEndPoint:"//trk.analytics.wisetracker.co.kr/web/v1/dataRcv.do",
+    adClkEndPoint:"//trk.analytics.wisetracker.co.kr/ldsys/v1/clickDataRcv.do",
+    adLandingEndPoint:"//app.wisetracker.co.kr",
+    includeUrl:"laflix.netlify.app",
+    excludeUrl:"",
+    referrerExpire:7
+});
+WDOTInitialization.init();
+let WDOT = window.WDOT;
 
 ReactDOM.render(<App />, document.getElementById('root'));
