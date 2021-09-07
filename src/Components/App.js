34e2e512b0ff116df3f83fcc21f1bdb7a8bea94e
lattepapa <whitelatte46@gmail.com>
2021-09-07 18:24:38 +0900

    add script

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,8 +2,27 @@ import React from "react"
 import Router from "Components/Router"
 import Header from "Components/Header"
 import GlobalStyles from "Components/GlobalStyles"
+import { WDOTInitialization } from "dop-website-sdk";
 
 function App() {
+  
+  useEffect(() => {
+     
+    WDOTInitialization.setConfig({
+        serviceNumber:20045,
+        dotAccessToken:"AgaCWt+E9pKnx5vijC6ZBQ1Eu7eyI7SKp7KIHoxbthlhH08GoUum0K2kmJg7/HuRARcv8D89eyGNzszJgiIzdg==",
+        combackUserLimitDays:14,
+        dotEndPoint:"//trk.analytics.wisetracker.co.kr/web/v1/dataRcv.do",
+        adClkEndPoint:"//trk.analytics.wisetracker.co.kr/ldsys/v1/clickDataRcv.do",
+        adLandingEndPoint:"//app.wisetracker.co.kr",
+        includeUrl:"laflix.netlify.app",
+        excludeUrl:"",
+        referrerExpire:7
+    });
+    WDOTInitialization.init();
+  }, []);
+
+  
   return (
     <>
       <Router />
